fe0612e563ba482c4155d0cebda6cb53f1b39d93
Roman <musinrr@gmail.com>
2021-05-08 14:21:26 +0100

    Prefix all constants with package name

--- android/src/main/java/com/emekalites/react/alarm/notification/Constants.java
+++ android/src/main/java/com/emekalites/react/alarm/notification/Constants.java
@@ -4,12 +4,11 @@ class Constants {
     static final String TAG = "RNAlarmNotification";
 
     // TODO convert to action
-    static final String ADD_INTENT = "com.emekalites.react.alarm.notification.Constants.ADD_INTENT";
+    static final String ADD_INTENT = "com.emekalites.react.alarm.notification.ADD_INTENT";
 
-    static final String NOTIFICATION_ID = "com.emekalites.react.alarm.notification.Constants.NOTIFICATION_ID";
+    static final String NOTIFICATION_ID = "com.emekalites.react.alarm.notification.NOTIFICATION_ID";
 
-    // TODO prefix with package name
-    static final String NOTIFICATION_ACTION_DISMISS = "ACTION_DISMISS";
-    static final String NOTIFICATION_ACTION_SNOOZE = "ACTION_SNOOZE";
-    static final String NOTIFICATION_ACTION_CLICK = "ACTION_CLICK";
+    static final String NOTIFICATION_ACTION_DISMISS = "com.emekalites.react.alarm.notification.ACTION_DISMISS";
+    static final String NOTIFICATION_ACTION_SNOOZE = "com.emekalites.react.alarm.notification.ACTION_SNOOZE";
+    static final String NOTIFICATION_ACTION_CLICK = "com.emekalites.react.alarm.notification.ACTION_CLICK";
 }
