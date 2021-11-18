50d3c044f0164d0fe27015623e8bc51f4acce5c7
tarobun <tarobun0@gmail.com>
2021-11-18 11:42:23 +1100

    Add type declarations

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+export default ReactNativeAN;
+
+declare namespace ReactNativeAN {
+    function scheduleAlarm(details: any): Promise<any>;
+    function sendNotification(details: any): void;
+    function deleteAlarm(id: any): void;
+    function deleteRepeatingAlarm(id: any): void;
+    function stopAlarmSound(): any;
+    function removeFiredNotification(id: any): void;
+    function removeAllFiredNotifications(): void;
+    function getScheduledAlarms(): Promise<any>;
+    function requestPermissions(permissions: any): Promise<any>;
+    function checkPermissions(callback: any): void;
+    function parseDate(rawDate: any): string;
+}
